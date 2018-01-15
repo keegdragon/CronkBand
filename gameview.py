df614188fdc3dc55bba656ce13b49191a26f4d5f
keegdragon <kpatterson@dyndrite.com>
2018-01-14 19:02:28 -0800

    Added screen logic, added a little incorrect event stuff

--- gameview.py
+++ gameview.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.event import Event, event_name, wait
 pygame.init()
 
 class GameView:
@@ -6,12 +7,26 @@ class GameView:
 
     #TODO: Designate width and height in terms of unispace font
     def __init__(self, width=320, height=240):
-        """Width and height specified at initialization"""
+        """Width and height specified at initialization
+        Setting mode opens window"""
         self.size = width, height
+        self.displaySurface = pygame.display.set_mode(self.size)
+        self.font = pygame.font.Font(None, 60)
 
     def openWindow(self):
         """Opens window of designated size; returns window object"""
-        return pygame.display.set_mode(self.size)
+        return self.displaySurface
 
     def draw(self, env):
-        pass
+        ren = self.font.render(env, 0, (0,0,255), (0,0,0))
+        self.displaySurface.blit(ren, (10, 10))
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    testWindow = GameView(1024, 768)
+    #testWindow.openWindow()
+    testWindow.draw("hello world!")
+    pygame.event.wait()
+    testWindow.draw("you pressed a button!")
+    pygame.event.wait()
