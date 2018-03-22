58b0a879d1e94c929446fe75421abb1f7558bb09
keegdragon <kpatterson@dyndrite.com>
2018-03-21 22:11:15 -0700

    added rudimentary event handling, next step look into constant motion and more robust handling

--- gameview.py
+++ gameview.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.event import Event, event_name, wait
 pygame.init()
 
 class GameView:
@@ -19,6 +18,7 @@ class GameView:
 
     def draw(self, env):
         ren = self.font.render(env, 0, (0,0,255), (0,0,0))
+        self.displaySurface.fill(pygame.Color(0,0,0))
         self.displaySurface.blit(ren, (10, 10))
         pygame.display.flip()
 
@@ -26,7 +26,36 @@ class GameView:
 if __name__ == '__main__':
     testWindow = GameView(1024, 768)
     #testWindow.openWindow()
-    testWindow.draw("hello world!")
-    pygame.event.wait()
-    testWindow.draw("you pressed a button!")
-    pygame.event.wait()
+    game_over = False
+    pygame.event.set_blocked(pygame.ACTIVEEVENT)
+    pygame.event.set_blocked(pygame.VIDEORESIZE)
+    pygame.event.set_blocked(pygame.MOUSEMOTION)
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    testWindow.draw("Space bar pressed down.")
+                elif event.key == pygame.K_z:
+                    testWindow.draw("Z key pressed down.")
+                elif event.key == pygame.K_q:
+                    testWindow.draw("")
+                    my_rect = pygame.draw.rect(testWindow.displaySurface, pygame.Color(0,0,255), (10,10,10,10))
+                    pygame.display.flip()
+                elif event.key == pygame.K_UP:
+                    testWindow.draw("")
+                    my_rect = pygame.draw.rect(testWindow.displaySurface, pygame.Color(0, 0, 255), my_rect.move(0,-5))
+                    pygame.display.flip()
+                elif event.key == pygame.K_LEFT:
+                    testWindow.draw("")
+                    my_rect = pygame.draw.rect(testWindow.displaySurface, pygame.Color(0, 0, 255), my_rect.move(-5,0))
+                    pygame.display.flip()
+                elif event.key == pygame.K_RIGHT:
+                    testWindow.draw("")
+                    my_rect = pygame.draw.rect(testWindow.displaySurface, pygame.Color(0, 0, 255), my_rect.move(5, 0))
+                    pygame.display.flip()
+                elif event.key == pygame.K_DOWN:
+                    testWindow.draw("")
+                    my_rect = pygame.draw.rect(testWindow.displaySurface, pygame.Color(0, 0, 255), my_rect.move(0, 5))
+                    pygame.display.flip()
+                elif event.key == pygame.K_ESCAPE:
+                    game_over = True
